a5db83f63ffe95057c1ed9eada35361d6544f6d0
Alexander Carr <Alexanderc93@gmail.com>
2015-01-13 11:51:10 -0800

    add comment stating command to run this script; add sublime text settings; add .zshrc and iterm settings;

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh
 
+# command to run this script on the new machine is:
+# curl https://raw.githubusercontent.com/AlexanderGalen/migrate/master/setup.sh | sh
 
-#install homebrew and homebrew cask for managing packages
+# install homebrew and homebrew cask for managing packages
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install caskroom/cask/brew-cask
 
-#install various programs I need with brew/cask
+# install various programs I need with brew/cask
 brew install git
 brew cask install dropbox
 brew cask install 1password
@@ -26,15 +28,14 @@ brew cask install steam
 brew cask install flash-player
 brew cask install breach
 
-
-#install composer
+# install composer
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
-#install sublime text 3
+# install sublime text 3
 mkdir ~/install_temp
 cd ~/install_temp
-curl -L -o "Sublime Text 3.dmg" "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203065.dmg"
+curl -fsSL -o "Sublime Text 3.dmg" "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203065.dmg"
 hdiutil mount -nobrowse "Sublime Text 3.dmg"
 cp -R "/Volumes/Sublime Text/Sublime Text.app" /Applications
 hdiutil unmount "/Volumes/Sublime Text/Sublime Text.app"
@@ -42,11 +43,16 @@ rm "Sublime Text 3.dmg"
 rm ~/install_temp
 cd ~
 
-#install oh-my-zsh
-curl -L http://install.ohmyz.sh | sh
+# symlink sublime text settings from dropbox
+cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
+rm -r User
+ln -s ~/Dropbox/Sublime/User
 
-#TODO
-#Add copying of .zshrc
-#cp source ~/.zshrc
-#Add copying of iterm prefs to ~/Library/Preferences
-#cp source ~/Library/Preferences/com.googlecode.iterm2.plist
+# install oh-my-zsh
+curl -fsSL http://install.ohmyz.sh | sh
+
+# get .zshrc and iterm prefs from from github
+curl -fsSL -o ~/.zshrc https://raw.githubusercontent.com/AlexanderGalen/migrate/master/.zshrc
+curl -fsSL -o ~/Library/Preferences/com.googlecode.iterm2.plist https://github.com/AlexanderGalen/migrate/blob/master/prefs/com.googlecode.iterm2.plist?raw=true
+
+
